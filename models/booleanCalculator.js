324d18b30954172dab7f76f99689eea2a9d24f4c
Kate <kate.smith@digital.justice.gov.uk>
2023-08-04 16:48:20 +0100

    works for edge cases but needs big clean up

--- models/booleanCalculator.js
+++ models/booleanCalculator.js
@@ -20,6 +20,9 @@ class BooleanCalculator {
             else if (val === "TRUE") {
                 inputArray[ key ] = true;
             }
+            else if (val === "") {
+                inputArray[ key ] = null;
+            }
         }
         return inputArray
     }
@@ -27,24 +30,59 @@ class BooleanCalculator {
     calculateForParenthesis(inputString) {
         let bothInputArrays = this.breakStringIntoSubArrays(inputString);
         let secondArray = []
+        console.log('input array 1')
+        console.log(this.mapObjectValues(bothInputArrays[0].split(" ")))
         let inputArray1 = this.mapObjectValues(bothInputArrays[0].split(" "))
         let sortedArray = this.sortArrayIntoCategories(inputArray1)
         let finalOutcome1 = this.getFinalOutcome(sortedArray)
+
         secondArray.push(finalOutcome1)
+        secondArray = secondArray.filter(function(item) {
+            return item !== null
+        })
         secondArray.push(this.mapObjectValues(bothInputArrays[1].split(" ")))
+        console.log('secondArray')
+        console.log(secondArray.flat())
         let k = this.sortArrayIntoCategories(secondArray.flat())
+        console.log('k')
+        console.log(k)
+        // k.forEach((element) => {
+        //     console.log('k in element')
+        //     console.log(k)
+        //     if (element !== 'NOT' && element !== null) {
+        //         console.log('time to return')
+        //         return this.getSingleValue(element, true)
+        //     }
+        // })
+
         return this.getFinalOutcome(k)
+
     }
 
     breakStringIntoSubArrays(inputString){
+        let a = []
         const inputStringForParenthesis = inputString.slice([inputString.indexOf("(")+1], [inputString.indexOf(")")])
-        const restOfInputString = inputString.replace(inputStringForParenthesis, "");
-        return [inputStringForParenthesis, restOfInputString.replace("() ", "")]
+        console.log('1')
+        console.log(inputStringForParenthesis)
+        let restOfInputString = inputString.replace(inputStringForParenthesis, "");
+        console.log('2')
+        console.log(restOfInputString)
+
+        let restOfInputStringW = restOfInputString.replace(" ()", "");
+        restOfInputStringW = restOfInputStringW.replace("()", "");
+        console.log("3")
+        console.log(restOfInputStringW)
+        console.log("inputStringForParenthesis")
+        console.log(inputStringForParenthesis)
+        a.push(inputStringForParenthesis)
+        a.push(restOfInputStringW)
+        console.log(a)
+        return [inputStringForParenthesis, restOfInputStringW]
     }
 
     removeSoloBools(inputArray) {
         let sortedArray = []
-
+        console.log('in solo bools')
         for(let i = 0; i < inputArray.length; i++) {
             if ((inputArray[i - 1] !== "AND") && (inputArray[i - 1] !== "OR") && (inputArray[i - 1] !== "NOT")) {
                 if ((inputArray[i + 1] !== "AND") && (inputArray[i + 1] !== "OR") && (inputArray[i + 1] !== "NOT")) {
@@ -58,22 +96,39 @@ class BooleanCalculator {
     }
 
     sortArrayIntoCategories(inputArray) {
-        let sortedArray = this.removeSoloBools(inputArray)
-        let sortedArrayRound1 = this.countAllIndividualElements(sortedArray, inputArray, "NOT")
-        let sortedArrayRound2 = this.countAllIndividualElements(sortedArrayRound1, inputArray, "AND")
-        return this.countAllIndividualElements(sortedArrayRound2, inputArray, "OR");
+        let newArray = []
+        if (inputArray.length === 2) {
+            if (inputArray.includes('NOT')) {
+                inputArray.forEach((element) => {
+                    if ((element !== 'NOT') && (element !== null)) {
+                        newArray.push(this.getSingleValue(element, true))
+                    }
+                })
+            }
+        }
+
+        let returnedElements = this.removeSoloBools(inputArray)
+        if (returnedElements.length >= 1) {
+            newArray.push(returnedElements)
+        }
+        returnedElements = this.countAllIndividualElements(inputArray, "NOT")
+        if (returnedElements.length >= 1) {
+            newArray.push(returnedElements)
+        }
+        returnedElements = this.countAllIndividualElements(inputArray, "AND")
+        if (returnedElements.length >= 1) {
+            newArray.push(returnedElements)
+        }
+        returnedElements = this.countAllIndividualElements(inputArray, "OR")
+        if (returnedElements.length >= 1) {
+            newArray.push(returnedElements)
+        }
+        return newArray.flat();
     }
 
     getFinalOutcome(sortedArray) {
-        let numberOfTrue = 0;
-        let numberOfFalse = 0;
-        sortedArray.forEach((element) => {
-            if (element === true) {
-                numberOfTrue++;
-            } else if(element === false) {
-                numberOfFalse++;
-            }
-        });
+        let numberOfTrue = sortedArray.filter(x => x===true).length
+        let numberOfFalse = sortedArray.filter(x => x===false).length;
         return numberOfTrue >= numberOfFalse
     }
 
@@ -83,7 +138,7 @@ class BooleanCalculator {
         } else if (bool === false) {
             return reversed !== false;
         }
-        return null;
+         return null;
     }
     calculateAndOrValues(elementArray, joiner) {
         if (elementArray.includes(joiner)) {
@@ -100,14 +155,17 @@ class BooleanCalculator {
         }
     }
 
-    countAllIndividualElements(sortedArray, inputArray, searchText) {
+    countAllIndividualElements(inputArray, searchText) {
+        let sortedArray = []
         let numberOfElementInArray = inputArray.filter(x => x===searchText).length
 
         for(let i = 0; i < numberOfElementInArray; i++) {
             let indexOfElement = inputArray.indexOf(searchText);
              if (searchText === "NOT") {
-                 const result = this.getSingleValue(inputArray[indexOfElement+1], true)
-                 sortedArray.push(result)
+                 if (inputArray[indexOfElement+1] !== null) {
+                     const result = this.getSingleValue(inputArray[indexOfElement+1], true)
+                     sortedArray.push(result)
+                 }
              } else {
                  const result = this.calculateAndOrValues([inputArray[indexOfElement-1], inputArray[indexOfElement], inputArray[indexOfElement+1]], searchText)
                  sortedArray.push(result)
